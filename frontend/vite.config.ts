decebdbbe6e6dcd045acb9912d76e6332c686b0e
Luca <dalessandroluca1@gmail.com>
2025-11-16 18:19:25 +0100

    fix allowed hosts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 const API_BASE = process.env.VITE_API_BASE || "http://localhost:3000";
+const API_DOMAIN = process.env.VITE_DOMAIN || "";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["deviceportal.lucadalessandro.freeddns.org","lolcalhost"],
+    allowedHosts: [API_DOMAIN,"localhost"],
     port: 5173,
     proxy: {
       "/api": {
